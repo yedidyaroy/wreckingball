a40b817a355cbfc012cef184c9d4aacd02dd76ac
yedidyaroy <76555587+yedidyaroy@users.noreply.github.com>
2021-06-29 18:18:34 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,24 @@
+class Ball{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'frictionAir':0.005,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        strokeWeight(4);
+        stroke("black");
+        fill("grey");
+        ellipseMode(RADIUS);
+        ellipse( 0, 0, this.width, this.height);
+        pop();
+      }
+}
